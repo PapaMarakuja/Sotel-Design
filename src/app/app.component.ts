e6203e0c02a05db556ccc30cdef1e3abf493a184
rafael <rafael@dimensaosistemas.com>
2021-12-29 09:31:20 -0300

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-root',
-	template: `<router-outlet></router-outlet>
-		<!--<app-loading></app-loading> -->`,
+	template: `<router-outlet></router-outlet>`,
 	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {}
