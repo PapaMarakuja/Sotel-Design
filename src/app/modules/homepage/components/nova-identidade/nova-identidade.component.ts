1062b99ebe21192b6602a9c0699ad15e9cc1c83f
Rafael Senac <vampirorp13@gmail.com>
2022-04-27 21:34:39 -0300

    Footer and minor changes

---
+++ src/app/modules/homepage/components/nova-identidade/nova-identidade.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nova-identidade',
+  templateUrl: './nova-identidade.component.html',
+  styleUrls: ['./nova-identidade.component.scss']
+})
+export class NovaIdentidadeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/homepage/components/skills/skills.component.ts
+++ src/app/modules/homepage/components/skills/skills.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
 	selector: 'app-skills',
 	templateUrl: './skills.component.html',
-	styleUrls: ['./skills.component.scss']
+	styleUrls: ['./skills.component.scss'],
 })
 export class SkillsComponent implements OnInit {
 	skills = [
@@ -13,24 +13,24 @@ export class SkillsComponent implements OnInit {
 				{
 					cardTitle: 'Logo',
 					cardText: 'Faço logos nesse caralho',
-					cardIcon: 'uil uil-palette'
+					cardIcon: 'uil uil-palette',
 				},
 				{
 					cardTitle: 'Sub logos',
 					cardText: 'Faço sub logos nesse caralho',
-					cardIcon: 'uil uil-paint-tool	'
+					cardIcon: 'uil uil-paint-tool	',
 				},
 				{
 					cardTitle: `Marca d'água`,
 					cardText: `Faço marca d'água nesse caralho`,
-					cardIcon: 'uil uil-tear	'
+					cardIcon: 'uil uil-tear	',
 				},
 				{
 					cardTitle: 'Pacote completo',
 					cardText: `Faço pacote completo nesse caralho`,
-					cardIcon: 'uil uil-tear	'
-				}
-			]
+					cardIcon: 'uil uil-tear	',
+				},
+			],
 		},
 		{
 			title: 'Redes sociais',
@@ -38,25 +38,25 @@ export class SkillsComponent implements OnInit {
 				{
 					cardTitle: 'Capa de destaque',
 					cardText: 'Capinha pra caceta do destaque',
-					cardIcon: 'uil uil-instagram'
+					cardIcon: 'uil uil-instagram',
 				},
 				{
 					cardTitle: 'Perfil do insta',
 					cardText: 'Perfil pra caceta do insta',
-					cardIcon: 'uil uil-user-circle'
-				}
-			]
+					cardIcon: 'uil uil-user-circle',
+				},
+			],
 		},
 		{
 			title: 'Only fans',
 			cards: [
 				{
-					cardTitle: 'Pirocão do namorado',
-					cardText: 'Pack sugando a pica do namorado',
-					cardIcon: 'uil uil-18-plus'
-				}
-			]
-		}
+					cardTitle: 'aiugadsjhgsndjk',
+					cardText: 'Pack ',
+					cardIcon: 'uil uil-package',
+				},
+			],
+		},
 	];
 
 	constructor() {}
